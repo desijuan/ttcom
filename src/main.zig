fd6379ea59562d08f62d0cad63cee7e0bb50e5a1
Juan Desimoni <desijuan89@gmail.com>
2026-06-18 18:47:07 -0300

    Embed schema.sql

--- src/main.zig
+++ src/main.zig
@@ -6,7 +6,7 @@ const Config = @import("Config.zig");
 const Conn = @import("model/db/Conn.zig");
 const App = @import("gui/gtk/App.zig");
 
-pub fn main() (Config.ReadFromDiskError || Conn.OpenError || App.CreateError)!void {
+pub fn main() (Config.ReadFromDiskError || Conn.OpenError || Conn.ExecSchemaError || App.CreateError)!void {
     defer mem.a_deinit();
 
     const cfg: Config = try Config.readFromDisk();
@@ -14,6 +14,8 @@ pub fn main() (Config.ReadFromDiskError || Conn.OpenError || App.CreateError)!vo
 
     const conn: Conn = try Conn.open(cfg.db_filename);
     defer conn.close();
+
+    try conn.execSchema();
 
     const app: *const App = try App.create(cfg, conn);
     defer app.destroy();
--- src/model/db/Conn.zig
+++ src/model/db/Conn.zig
@@ -4,20 +4,14 @@ const log = std.log;
 const c = @import("c.zig").sqlite3;
 
 const mem = @import("../../mem.zig");
-const utils = @import("../../utils.zig");
-
-const path_db_schema = "src/model/db/schema.sql";
 
 const Conn = @This();
 
 p_db: ?*c.sqlite3,
 
-pub const OpenError = utils.ReadFileZError || error{ OpenFailed, SchemaExecFailed };
+pub const OpenError = error{OpenFailed};
 
 pub fn open(file_name: [:0]const u8) OpenError!Conn {
-    const schema_str: [:0]const u8 = try utils.readFileZ(mem.a, path_db_schema);
-    defer mem.a.free(schema_str);
-
     log.info("SQLite v{s}", .{c.sqlite3_libversion()});
 
     var p_db: ?*c.sqlite3 = null;
@@ -38,28 +32,34 @@ pub fn open(file_name: [:0]const u8) OpenError!Conn {
 
     log.info("Connected to database: {s}", .{file_name});
 
-    var err_msg: [*c]u8 = null;
-    if (c.SQLITE_OK != c.sqlite3_exec(p_db, schema_str, null, null, &err_msg)) {
-        defer c.sqlite3_free(err_msg);
-        log.err("failed to apply schema: {s}", .{err_msg});
-        return error.SchemaExecFailed;
-    }
-
     return Conn{ .p_db = p_db };
 }
 
 pub const CloseError = error{CloseFailed};
 
-// Q: Do I have to free the err_msg here?
 pub fn close(self: Conn) void {
     if (c.SQLITE_OK != c.sqlite3_close(self.p_db)) {
         log.err(
             "Error closing the database: {s}",
             .{c.sqlite3_errmsg(self.p_db)},
         );
+        return;
     }
 
     log.info("Database closed", .{});
+}
+
+pub const schema = @embedFile("schema.sql");
+
+pub const ExecSchemaError = error{SchemaExecFailed};
+
+pub fn execSchema(self: Conn) ExecSchemaError!void {
+    var err_msg: [*c]u8 = null;
+    if (c.SQLITE_OK != c.sqlite3_exec(self.p_db, schema, null, null, &err_msg)) {
+        defer c.sqlite3_free(err_msg);
+        log.err("failed to apply schema: {s}", .{err_msg});
+        return error.SchemaExecFailed;
+    }
 }
 
 /// The returned slice is valid only while the connection remains open.
